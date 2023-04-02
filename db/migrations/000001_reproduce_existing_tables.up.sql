561b137ff1e4a5fee3f1179d2e972f7563b4313d
hayato24s <engr.hayato24s@gmail.com>
2023-04-03 08:56:20 +0900

    Remove constrain name

--- db/migrations/000001_reproduce_existing_tables.up.sql
+++ db/migrations/000001_reproduce_existing_tables.up.sql
@@ -1,15 +1,11 @@
 BEGIN;
 
-CREATE TYPE user_authentications_provider_enum AS ENUM (
-    'Google',
-    'Twitter',
-    'Apple'
-);
+CREATE TYPE user_authentications_provider_enum AS ENUM ('Google', 'Twitter', 'Apple');
 
 CREATE TABLE users (
     id uuid NOT NULL,
     "createdAt" timestamp without time zone DEFAULT NOW() NOT NULL,
-    CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE user_authentications (
@@ -17,8 +13,8 @@ CREATE TABLE user_authentications (
     user_id uuid,
     provider user_authentications_provider_enum NOT NULL,
     social_id varchar NOT NULL,
-    CONSTRAINT "PK_5357fb1162b50b926c77290c8bc" PRIMARY KEY (id),
-    CONSTRAINT "FK_163ff5c9a502621798f57606e80" FOREIGN KEY (user_id) REFERENCES users(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 COMMIT;
